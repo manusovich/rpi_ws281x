1de56ee9983253626a66614a1ee8e4729d52933f
Alex Manusovich <alex.manusovich@mozido.com>
2015-01-18 21:58:44 -0600

    4

--- ws2811.c
+++ ws2811.c
@@ -683,11 +683,7 @@ int ws2811_render(ws2811_t *ws2811)
 {
     volatile uint8_t *pwm_raw = ws2811->device->pwm_raw;
     int maxcount = max_channel_led_count(ws2811);
-
-    int bitposdef = 32;
-
-
-    int bitpos = bitposdef;
+    int bitpos = 31;
     int i, j, k, l, chan;
 
     for (chan = 0; chan < RPI_PWM_CHANNELS; chan++)         // Channel
@@ -737,7 +733,7 @@ int ws2811_render(ws2811_t *ws2811)
                             // Every other word is on the same channel
                             wordpos += 2;
 
-                            bitpos = bitposdef;
+                            bitpos = 31;
                         }
                     }
                 }
