1ad0f39fd51adc32143244739c83fe51af4f1c95
Alex Manusovich <alex.manusovich@mozido.com>
2015-02-12 21:10:34 -0600

    4

--- main.c
+++ main.c
@@ -217,11 +217,11 @@ void matrix_render_exciter(void) {
         matrix[pos][y] = getXRGB(up(forecast_color(y), 2));
 
         if (dotposition[y] >= WIDTH - 1 && dotdirection[y] > 0) {
-            dotdirection[y] = - wind[y] / 400;
+            dotdirection[y] = - (float) wind[y] / 800;
         }
 
         if (dotposition[y] <= 0 && dotdirection[y] < 0) {
-            dotdirection[y] = wind[y] / 400;
+            dotdirection[y] = (float) wind[y] / 800;
         }
 
         dotposition[y] = dotposition[y] + dotdirection[y];
