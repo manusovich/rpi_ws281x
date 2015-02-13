1334be56e8eb1220be32144735d92beeba64db74
Alex Manusovich <alex.manusovich@mozido.com>
2015-02-12 21:16:52 -0600

    4

--- main.c
+++ main.c
@@ -176,7 +176,7 @@ void matrix_fade() {
             struct RGB rgb = getRGB(forecast_color(y));
             struct XRGB xrgb = matrix[x][y];
 
-            double d = 0.95;
+            double d = 0.98;
 
             if (xrgb.r > rgb.r) {
                 xrgb.r = (float) (xrgb.r * d);
